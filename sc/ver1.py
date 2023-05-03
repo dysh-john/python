a143ad9423e758bd4b991cd3a96e6da27d1bdaba
john <105053462+dysh-john@users.noreply.github.com>
2023-05-03 12:42:25 +0800

    Update ver1.py

--- sc/ver1.py
+++ sc/ver1.py
@@ -30,175 +30,181 @@ if not ask_login_tk:
     exit()
 
 
-
-sleep(1)
-driver.get("https://sdms.dysh.tyc.edu.tw/school/Roll_Call/Admin/RC/RC_card?title=%u5237%u5361%u9ede%u540dz")
-
-
-
+ask_continue = "y"
+while ask_continue == "Y" or ask_continue == "y":
+    sleep(1)
+    driver.get("https://sdms.dysh.tyc.edu.tw/school/Roll_Call/Admin/RC/RC_card?title=%u5237%u5361%u9ede%u540dz")
 
 
-# ------ Start go to data ------
+    # ------ Start go to data ------
 
-start_scrape_ask = input("開始抓取自學刷卡點名 (Y/y):")
-if start_scrape_ask == "y" or start_scrape_ask == "Y":
-    author_website = "yeh-john.github.io"
-else:
-    quit()
+    start_scrape_ask = input("開始抓取自學刷卡點名 (Y/y):")
+    if start_scrape_ask == "y" or start_scrape_ask == "Y":
+        author_website = "yeh-john.github.io"
+    else:
+        exit()
 
+    std_class = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[2]')
+    std_seatNum = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[3]')
+    std_number = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[1]')
+    std_name = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[4]')
+    std_status = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[5]/select/option[1]')
 
-std_class = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[2]')
-std_seatNum = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[3]')
-std_number = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[1]')
-std_name = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[4]')
-std_status = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[5]/select/option[1]')
-
-# Other std_status option
-std_statusA = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[6]/select/option[1]')
-
-
-Class = []
-SeatNum = []
-Number = []
-Name = []
-Status = []
-late_stdNum = []
+    # Other std_status option
+    std_statusA = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[6]/select/option[1]')
 
 
-def add_element():
-    for add_class in std_class:
-        Class.append(add_class.text)
-
-    for add_seatNum in std_seatNum:
-        SeatNum.append(add_seatNum.text)
-
-    for add_number in std_number:
-        Number.append(add_number.text)
-
-    for add_name in std_name:
-        Name.append(add_name.text)
-
-    for add_status in std_status:
-        Status.append(add_status.text)
+    Class = []
+    SeatNum = []
+    Number = []
+    Name = []
+    Status = []
+    late_stdNum = []
 
 
-def edit_list():
-    if '' in Class:
-        Class.remove('')
-    if '' in SeatNum:
-        SeatNum.remove('')
-    if '' in Number:
-        Number.remove('')
-    if '' in Name:
-        Name.remove('')
-    if '' in Status:
-        Status.remove('')
+    def add_element():
+        for add_class in std_class:
+            Class.append(add_class.text)
+
+        for add_seatNum in std_seatNum:
+            SeatNum.append(add_seatNum.text)
+
+        for add_number in std_number:
+            Number.append(add_number.text)
+
+        for add_name in std_name:
+            Name.append(add_name.text)
+
+        for add_status in std_status:
+            Status.append(add_status.text)
 
 
-
-def check_data():
-    # If Status's data is empty
-    if len(Status) < 1:
-        for add_statusA in std_statusA:
-            Status.append(add_statusA.text)
+    def edit_list():
+        if '' in Class:
+            Class.remove('')
+        if '' in SeatNum:
+            SeatNum.remove('')
+        if '' in Number:
+            Number.remove('')
+        if '' in Name:
+            Name.remove('')
         if '' in Status:
             Status.remove('')
 
 
-def convert_csv():
-    df = pd.DataFrame({'Class': Class, 'Seat': SeatNum, 'Number': Number, 'Name': Name, 'Status': Status})
+
+    def check_data():
+        # If Status's data is empty
+        if len(Status) < 1:
+            for add_statusA in std_statusA:
+                Status.append(add_statusA.text)
+            if '' in Status:
+                Status.remove('')
+
+
+    def convert_csv():
+        df = pd.DataFrame({'Class': Class, 'Seat': SeatNum, 'Number': Number, 'Name': Name, 'Status': Status})
+        sleep(1)
+        late_std = df[df['Status'] != '已刷卡']
+        late_std = late_std.reset_index(drop=True)
+        print(late_std)
+        # print("________________")
+
+        result_stdNum = late_std["Number"].to_list()
+
+        for add_result_stdNum in result_stdNum:
+            late_stdNum.append(add_result_stdNum)
+
+        return late_std
+
+
+    # Use def script
+    add_element()
     sleep(1)
-    late_std = df[df['Status'] != '已刷卡']
-    late_std = late_std.reset_index(drop=True)
-    print(late_std)
-    # print("________________")
-
-    result_stdNum = late_std["Number"].to_list()
-
-    for add_result_stdNum in result_stdNum:
-        late_stdNum.append(add_result_stdNum)
-
-    return late_std
-
-
-# Use def script
-add_element()
-sleep(1)
-edit_list()
-sleep(1)
-check_data()
-sleep(1)
-testA = convert_csv()
-ansA = testA
-
-
-
-# --------  Start get arrival time --------
-
-sleep(3)
-print("3秒後跳到抓到校時間頁面.....")
-driver.get("https://sdms.dysh.tyc.edu.tw/school/Roll_Call/admin/RC/Roll_Call2?title=%u9ede%u540d%u7ba1%u7406")
-
-
-arrivalTime = []
-arrivalStdnum = []
-
-
-
-def get_arrivalTime():
-    std_arrivalTime = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[4]')
-    std_arrivalStdnum = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[1]')
-
-    for add_arrivalTime in std_arrivalTime:
-        arrivalTime.append(add_arrivalTime.text)
-    for add_arrivalStdnum in std_arrivalStdnum:
-        arrivalStdnum.append(add_arrivalStdnum.text)
-
-    if '' in arrivalTime:
-        arrivalTime.remove('')
-    if '' in arrivalStdnum:
-        arrivalStdnum.remove('')
-    
-
-
-def convert_ArrivalCsv():
-    df_arrivalStd = pd.DataFrame({'Number': arrivalStdnum, 'arrivalTime': arrivalTime})
+    edit_list()
     sleep(1)
-    df_arrivalStd = df_arrivalStd.reset_index(drop=True)
-
-
-
-    fillter_data = df_arrivalStd[df_arrivalStd['Number'].isin(late_stdNum)]
-    fillter_data = fillter_data.reset_index(drop=True)
-    
-
-    return fillter_data
-
-
-geting_arrivalStd = input("是否開始抓取到校時間?  Y / y :")
-num = 1
-while geting_arrivalStd == "y" or geting_arrivalStd == "Y":
+    check_data()
     sleep(1)
-    get_arrivalTime()
-    print("已抓取完第"+str(num)+"個班級......")
-    num = num + 1
+    testA = convert_csv()
+    ansA = testA
+
+
+
+    # --------  Start get arrival time --------
+
+    sleep(3)
+    print("3秒後跳到抓到校時間頁面.....")
+    driver.get("https://sdms.dysh.tyc.edu.tw/school/Roll_Call/admin/RC/Roll_Call2?title=%u9ede%u540d%u7ba1%u7406")
+
+
+    arrivalTime = []
+    arrivalStdnum = []
+
+
+
+    def get_arrivalTime():
+        std_arrivalTime = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[4]')
+        std_arrivalStdnum = driver.find_elements(By.XPATH, '/html/body/div[2]/form/div[4]/table/tbody/tr/td[1]')
+
+        for add_arrivalTime in std_arrivalTime:
+            arrivalTime.append(add_arrivalTime.text)
+        for add_arrivalStdnum in std_arrivalStdnum:
+            arrivalStdnum.append(add_arrivalStdnum.text)
+
+        if '' in arrivalTime:
+            arrivalTime.remove('')
+        if '' in arrivalStdnum:
+            arrivalStdnum.remove('')
+        
+
+
+    def convert_ArrivalCsv():
+        df_arrivalStd = pd.DataFrame({'Number': arrivalStdnum, 'arrivalTime': arrivalTime})
+        sleep(1)
+        df_arrivalStd = df_arrivalStd.reset_index(drop=True)
+
+
+
+        fillter_data = df_arrivalStd[df_arrivalStd['Number'].isin(late_stdNum)]
+        fillter_data = fillter_data.reset_index(drop=True)
+        
+
+        return fillter_data
+
+
     geting_arrivalStd = input("是否開始抓取到校時間?  Y / y :")
+    num = 1
+    while geting_arrivalStd == "y" or geting_arrivalStd == "Y":
+        sleep(1)
+        get_arrivalTime()
+        print("已抓取完第"+str(num)+"個班級......")
+        num = num + 1
+        geting_arrivalStd = input("是否開始抓取到校時間?  Y / y :")
 
 
 
 
 
-testB = convert_ArrivalCsv()
-ansB = testB
+    testB = convert_ArrivalCsv()
+    ansB = testB
 
 
 
-print("----- This is result -----")
-result_data = pd.merge(ansA, ansB, on='Number')
-print(result_data)
+    print("----- This is result -----")
+    result_data = pd.merge(ansA, ansB, on='Number')
+    print(result_data)
 
 
 
-sleep(3)
-print("")
-input("Press ENTER to exit the program....")
+    sleep(3)
+    print("")
+    ask_continue = input("Do you want to continue to use the program? (Y/y)")
+    if ask_continue == "Y" or ask_continue == "y":
+        "After 3 seconds start new scarping...."
+        sleep(3)
+        print("")
+        print("----- Start new scraping -----")
+    else:
+        exit()
+
+print("Done.....")
